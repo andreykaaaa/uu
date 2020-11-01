b9589f2966105e37c238852d2370a27f92086013
andreykaaaa <73058771+andreykaaaa@users.noreply.github.com>
2020-11-01 12:55:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+var = int(input("Введите значение"))
+
+
+if var == 1:
+    print("Январь")
+if var == 2:
+    print("Февраль")
+if var == 3:
+    print("Март")
+if var == 4:
+    print("Апрель")
+if var == 5:
+    print("Май")
+if var == 6:
+    print("Июнь")
+if var == 7:
+    print("Июль")
+if var == 8:
+    print("Август")
+if var == 9:
+    print("Сентябрь")
+if var == 10:
+    print("Октябрь")
+if var == 11:
+    print("Ноябрь")
+if var == 12:
+    print("Декабрь")
